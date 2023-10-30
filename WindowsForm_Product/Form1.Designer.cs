9e3f3993555fe9150d4db91d9b5a24543613b2a0
sandesh kshirsagar <sandeshkshirsagar505@gmail.com>
2023-10-30 16:21:53 +0530

    Add project files.

---
+++ WindowsForm_Product/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace WindowsForm_Product
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblProductid = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.btnCreateDirectory = new System.Windows.Forms.Button();
+            this.btnCreateFile = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnStreamWrite = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnSteamRead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblProductid
+            // 
+            this.lblProductid.AutoSize = true;
+            this.lblProductid.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lblProductid.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.lblProductid.Location = new System.Drawing.Point(47, 38);
+            this.lblProductid.Name = "lblProductid";
+            this.lblProductid.Size = new System.Drawing.Size(18, 16);
+            this.lblProductid.TabIndex = 0;
+            this.lblProductid.Text = "id";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.lblName.Location = new System.Drawing.Point(47, 110);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(44, 16);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.lblPrice.Location = new System.Drawing.Point(47, 181);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(38, 16);
+            this.lblPrice.TabIndex = 2;
+            this.lblPrice.Text = "Price";
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(138, 34);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(121, 20);
+            this.txtid.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(138, 106);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(121, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(138, 177);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(121, 20);
+            this.txtPrice.TabIndex = 5;
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnWrite.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWrite.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnWrite.Location = new System.Drawing.Point(76, 242);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(78, 32);
+            this.btnWrite.TabIndex = 6;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = false;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // btnCreateDirectory
+            // 
+            this.btnCreateDirectory.BackColor = System.Drawing.Color.LightCoral;
+            this.btnCreateDirectory.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreateDirectory.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnCreateDirectory.Location = new System.Drawing.Point(302, 40);
+            this.btnCreateDirectory.Name = "btnCreateDirectory";
+            this.btnCreateDirectory.Size = new System.Drawing.Size(90, 41);
+            this.btnCreateDirectory.TabIndex = 7;
+            this.btnCreateDirectory.Text = "Create Directory";
+            this.btnCreateDirectory.UseVisualStyleBackColor = false;
+            this.btnCreateDirectory.Click += new System.EventHandler(this.btnCreateDirectory_Click);
+            // 
+            // btnCreateFile
+            // 
+            this.btnCreateFile.BackColor = System.Drawing.Color.LightCoral;
+            this.btnCreateFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreateFile.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnCreateFile.Location = new System.Drawing.Point(302, 106);
+            this.btnCreateFile.Name = "btnCreateFile";
+            this.btnCreateFile.Size = new System.Drawing.Size(90, 41);
+            this.btnCreateFile.TabIndex = 8;
+            this.btnCreateFile.Text = "Create File";
+            this.btnCreateFile.UseVisualStyleBackColor = false;
+            this.btnCreateFile.Click += new System.EventHandler(this.btnCreateFile_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRead.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnRead.Location = new System.Drawing.Point(172, 242);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(78, 32);
+            this.btnRead.TabIndex = 9;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = false;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnStreamWrite
+            // 
+            this.btnStreamWrite.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnStreamWrite.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnStreamWrite.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStreamWrite.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnStreamWrite.Location = new System.Drawing.Point(426, 211);
+            this.btnStreamWrite.Name = "btnStreamWrite";
+            this.btnStreamWrite.Size = new System.Drawing.Size(106, 35);
+            this.btnStreamWrite.TabIndex = 10;
+            this.btnStreamWrite.Text = "Stream Write";
+            this.btnStreamWrite.UseVisualStyleBackColor = false;
+            this.btnStreamWrite.Click += new System.EventHandler(this.btnStreamWrite_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(417, 40);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(281, 137);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // btnSteamRead
+            // 
+            this.btnSteamRead.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSteamRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSteamRead.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSteamRead.Location = new System.Drawing.Point(564, 211);
+            this.btnSteamRead.Name = "btnSteamRead";
+            this.btnSteamRead.Size = new System.Drawing.Size(106, 35);
+            this.btnSteamRead.TabIndex = 12;
+            this.btnSteamRead.Text = "Stream Read";
+            this.btnSteamRead.UseVisualStyleBackColor = false;
+            this.btnSteamRead.Click += new System.EventHandler(this.btnSteamRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSteamRead);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnStreamWrite);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnCreateFile);
+            this.Controls.Add(this.btnCreateDirectory);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblProductid);
+            this.Name = "Form1";
+            this.Text = "Product";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblProductid;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button btnCreateDirectory;
+        private System.Windows.Forms.Button btnCreateFile;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnStreamWrite;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnSteamRead;
+    }
+}
+
---
+++ WindowsForm_Product/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsForm_Product
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateDirectory_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = @"C:\TestFolder Product";
+                DirectoryInfo info = new DirectoryInfo(path);
+
+
+                if (info.Exists)
+                {
+                    MessageBox.Show("Directory already exists...");
+                }
+                else
+                {
+                    info.Create();
+                    MessageBox.Show("Directory Created...");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnCreateFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = @"C:\TestFolder Product\Product.dat"; //.dat-->Data file(binary)
+                FileInfo file = new FileInfo(path);
+
+                if (file.Exists)
+                {
+                    MessageBox.Show("File already exists...");
+                }
+                else
+                {
+                    file.Create();
+                    MessageBox.Show("File Created...");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"C:\TestFolder Product\Product.dat", FileMode.Create, FileAccess.Write);
+                BinaryWriter bw = new BinaryWriter(fs);
+                bw.Write(Convert.ToInt32(txtid.Text));
+                bw.Write(txtName.Text);
+                bw.Write(Convert.ToDouble(txtPrice.Text));
+                bw.Close();
+                fs.Close();
+                MessageBox.Show("Done");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"C:\TestFolder Product\Product.dat", FileMode.Open, FileAccess.Read);
+                BinaryReader br = new BinaryReader(fs);
+                txtid.Text = br.ReadInt32().ToString();
+                txtName.Text = br.ReadString();
+                txtPrice.Text = br.ReadDouble().ToString();
+                br.Close();
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnStreamWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"C:\TestFolder Product\data.txt", FileMode.Create, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine(richTextBox1.Text);
+                sw.Close();
+                fs.Close();
+                MessageBox.Show("Done");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSteamRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"C:\TestFolder Product\data.txt", FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(fs);
+                richTextBox1.Text = sr.ReadToEnd();
+                sr.Close();
+                fs.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
